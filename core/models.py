a853723263331229f09b636a7370863e0491e7e7
ilyanadelyaev <nadelyaev.ilya@gmail.com>
2015-12-06 18:18:21 +0300

    core.models: add docstrings and unicode

--- core/models.py
+++ core/models.py
@@ -4,13 +4,24 @@ import core.tools.enum
 
 
 class Tag(django.db.models.Model):
+    """
+    Tags for events and places
+    """
     tag = django.db.models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.tag
 
 
 ####################
 
 
 class EventType(core.tools.enum.Enum):
+    """
+    Event type enum to compact type field
+    Please update manually
+    Unknown types will store in EventData
+    """
     unknown = 0
     other = 1
 
@@ -21,6 +32,11 @@ class EventType(core.tools.enum.Enum):
 
 
 class Event(django.db.models.Model):
+    """
+    Base event model
+
+    ext_id: PARSER:EXTERNAL_ID string
+    """
     ext_id = django.db.models.CharField(db_index=True, max_length=100)
     type = django.db.models.PositiveSmallIntegerField(
         choices=EventType())
@@ -33,35 +49,63 @@ class Event(django.db.models.Model):
         null=True, blank=True)
 
     def type_str(self):
+        """
+        type to str via EventType
+        """
         return EventType(self.type)
+
+    def __unicode__(self):
+        return '[{} : {}] ({}) {}'.format(
+            self.id, self.ext_id, self.type, self.title)
 
 
 class EventTags(django.db.models.Model):
     """
-    to search via tags
+    Search events via tags
     """
     event = django.db.models.ForeignKey(Event)
     tag = django.db.models.ForeignKey(Tag)
 
+    def __unicode__(self):
+        return '[{}] {}'.format(
+            self.event.id, self.tag.tag)
+
 
 class EventImages(django.db.models.Model):
+    """
+    Store all event images links here
+    """
     event = django.db.models.ForeignKey(Event)
     image = django.db.models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return '[{}] {}'.format(
+            self.event.id, self.image)
 
 
 class EventData(django.db.models.Model):
     """
-    mics keys in events
+    Unknown event data
+    Do not miss anything
     """
     event = django.db.models.ForeignKey(Event)
     key = django.db.models.CharField(max_length=20)
     value = django.db.models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return '[{}] {} = {}'.format(
+            self.event.id, self.key, self.value)
 
 
 ####################
 
 
 class PlaceType(core.tools.enum.Enum):
+    """
+    Place type enum to compact type field
+    Please update manually
+    Unknown types will store in PlaceData
+    """
     unknown = 0
     other = 1
 
@@ -72,10 +116,21 @@ class PlaceType(core.tools.enum.Enum):
 
 
 class City(django.db.models.Model):
+    """
+    Keep city unque and compact storage
+    """
     name = django.db.models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Place(django.db.models.Model):
+    """
+    Base place model
+
+    ext_id: PARSER:EXTERNAL_ID string
+    """
     ext_id = django.db.models.CharField(db_index=True, max_length=100)
     type = django.db.models.PositiveSmallIntegerField(
         choices=PlaceType())
@@ -91,15 +146,34 @@ class Place(django.db.models.Model):
     geo_longitude = django.db.models.FloatField(null=True)
 
     def type_str(self):
+        """
+        type to str via PlaceType
+        """
         return PlaceType(self.type)
+
+    def __unicode__(self):
+        return '[{} : {}] ({}) {}'.format(
+            self.id, self.ext_id, self.type, self.title)
 
 
 class PlaceTags(django.db.models.Model):
+    """
+    Search places via tags
+    """
     place = django.db.models.ForeignKey(Place)
     tag = django.db.models.ForeignKey(Tag)
 
+    def __unicode__(self):
+        return '[{}] {}'.format(
+            self.place.id, self.tag.tag)
+
 
 class PhoneType(core.tools.enum.Enum):
+    """
+    Phone type enum to compact type field
+    Please update manually
+    Unknown types will store in PlaceData
+    """
     unknown = 0
     other = 1
 
@@ -110,18 +184,37 @@ class PhoneType(core.tools.enum.Enum):
 
 
 class PlacePhones(django.db.models.Model):
+    """
+    One place may have multiple phones
+    """
     place = django.db.models.ForeignKey(Place)
     type = django.db.models.PositiveSmallIntegerField(
         choices=PhoneType())
     phone = django.db.models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return '[{}] ({}) {}'.format(
+            self.place.id, self.type, self.phone)
+
 
 class PlaceMetros(django.db.models.Model):
+    """
+    One place may be located near multiple metro stations
+    """
     place = django.db.models.ForeignKey(Place)
     metro = django.db.models.CharField(max_length=30)
 
+    def __unicode__(self):
+        return '[{}] {}'.format(
+            self.place.id, self.metro)
+
 
 class WorkTimeType(core.tools.enum.Enum):
+    """
+    Work time type enum to compact type field
+    Please update manually
+    Unknown types will store in PlaceData
+    """
     unknown = 0
     other = 1
     openhours = 2
@@ -136,15 +229,30 @@ class WorkTimeType(core.tools.enum.Enum):
 
 
 class PlaceWorkTimes(django.db.models.Model):
+    """
+    Working time by type
+    For example "open hours" or "ticket office work"
+    """
     place = django.db.models.ForeignKey(Place)
     type = django.db.models.PositiveSmallIntegerField(
         choices=WorkTimeType())
     work_time = django.db.models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return '[{}] ({}) {}'.format(
+            self.place.id, self.type, self.work_time)
+
 
 class PlaceImages(django.db.models.Model):
+    """
+    Store all place images links here
+    """
     place = django.db.models.ForeignKey(Place)
     image = django.db.models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return '[{}] {}'.format(
+            self.place.id, self.image)
 
 
 class PlaceData(django.db.models.Model):
@@ -155,14 +263,27 @@ class PlaceData(django.db.models.Model):
     key = django.db.models.CharField(max_length=20)
     value = django.db.models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return '[{}] {} = {}'.format(
+            self.place.id, self.key, self.value)
+
 
 ####################
 
 
 class Schedule(django.db.models.Model):
+    """
+    Event time and place
+    """
     event = django.db.models.ForeignKey(Event)
     place = django.db.models.ForeignKey(Place)
     #
     date = django.db.models.DateField()
     start_time = django.db.models.TimeField(null=True, blank=True)
     end_time = django.db.models.TimeField(null=True, blank=True)
+
+    def __unicode__(self):
+        return '[{}] {} / {} : {} {}'.format(
+            self.id,
+            self.event.id, self.place.id,
+            self.date, self.start_time)
