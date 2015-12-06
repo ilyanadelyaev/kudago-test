8723e4e86dd4021c82ad5a2b2aa2d3f8958c5744
ilyanadelyaev <nadelyaev.ilya@gmail.com>
2015-12-06 15:17:37 +0300

    templates: add base and index

--- www/urls.py
+++ www/urls.py
@@ -6,5 +6,10 @@ import www.views
 urlpatterns = [
     django.conf.urls.url(r'^$', www.views.index, name='index'),
 
+    django.conf.urls.url(r'^event/(?P<pk>[0-9]+)/$', www.views.event, name='event'),
+    django.conf.urls.url(r'^event/$', www.views.events_list, name='events_list'),
+    django.conf.urls.url(r'^place/(?P<pk>[0-9]+)/$', www.views.place, name='place'),
+    django.conf.urls.url(r'^place/$', www.views.places_list, name='places_list'),
+
     django.conf.urls.url(r'^run_parsers$', www.views.run_parsers, name='run_parsers'),
 ]
--- www/views.py
+++ www/views.py
@@ -1,10 +1,30 @@
 import django.shortcuts
 
+import core.models
 import parsers.controller
 
 
 def index(request):
-    return django.shortcuts.render(request, 'www/index.html')
+    schedule = core.models.Schedule.objects.all()
+    return django.shortcuts.render(request, 'www/index.html', {
+        'schedule': schedule,
+    })
+
+
+def events_list(request):
+    pass
+
+
+def event(request, pk):
+    pass
+
+
+def places_list(request):
+    pass
+
+
+def place(request, pk):
+    pass
 
 
 def run_parsers(request):
