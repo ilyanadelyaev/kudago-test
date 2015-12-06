f60620ff091caaf057bf5fd77dd7d358c72d4fe4
ilyanadelyaev <nadelyaev.ilya@gmail.com>
2015-12-06 10:07:33 +0300

    add run_parsers dummy handle

--- www/urls.py
+++ www/urls.py
@@ -5,4 +5,6 @@ import www.views
 
 urlpatterns = [
     django.conf.urls.url(r'^$', www.views.index, name='index'),
+
+    django.conf.urls.url(r'^run_parsers$', www.views.run_parsers, name='run_parsers'),
 ]
--- www/views.py
+++ www/views.py
@@ -3,3 +3,7 @@ import django.shortcuts
 
 def index(request):
     return django.shortcuts.render(request, 'www/index.html')
+
+
+def run_parsers(request):
+    return django.shortcuts.render(request, 'www/run_parsers.html')
